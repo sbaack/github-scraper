0b590868ab0e563fd082a9034005d41a6c3dcc88
sbaack <cecep@posteo.de>
2015-09-21 15:17:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# GitHub scraper
+
+This repo contains a GitHub scraper I wrote for my research project on civic hacking. At the moment it is very much focused on *organizations* and their members, i.e. it only takes a list or organizations as its input, not normal user accounts. It generates CSV spreadsheets or [GEXF](http://gexf.net/format/) files to be used in network analysis software like [Gephi](https://gephi.github.io/). I tested it with Python 2.7.10 and 3.5.0, but I recommend using Python 3.
+
+The scraper can do the following:
+
+1. Get a list of the organizations' repositories (CSV).
+2. Get all contributors of the organizations' repositories as a spreadsheet and as a directed graph (CSV and GEXF).
+3. Get a list of the repositories of all the members of the organizations (CSV).
+4. Get information for each member of the organizations (CSV).
+5. Get a list of all the repositories starred by the members of the organizations (CSV).
+6. Generate a 'full' follower network. This will take all the members of the organizations and collect all their followers and all the users they are following. Will create a directed graph (GEXF).
+7. Generate a 'narrow' follower network. Works like 6 but only collects connections between the members of the organizations scraped by the user (GEXF).
+8. Generate a directed graph illustrating the membership structures (GEXF).
+
+## How to use
+
+1. Open `config.json` and add your GitHub user name and your [personal access token](https://github.com/settings/tokens) to access the GitHub API.
+2. Open `organizations.txt` and add the user account names of the organizations you want to scrape -- one organization per line! For example, if you want to scrape [mySociety](https://github.com/mysociety), [Open Knowledge](https://github.com/okfn), and [Ushahidi](https://github.com/ushahidi), your list will look like this:
+
+```
+mysociety
+okfn
+ushahidi
+```
+
+The results will be stored in the `data` subfolder.
---
+++ github_scraper.py
@@ -0,0 +1,264 @@
+# -*- coding: utf-8 -*-
+# To make code compatible with both Python 2 and 3: import __future__ print
+# function and map raw_input to input if Python 2 is used
+from __future__ import print_function
+try:
+    input = raw_input
+except NameError:
+    pass
+
+import json, csv, requests, time
+from sys import exit
+import networkx as nx
+# For Python 2: Using smart_str to deal with utf-8 encoded text in CSVs
+from django.utils.encoding import smart_str
+
+def start():
+    """Getting started by loading GitHub user name and API token, reading list
+    of organizations and letting user choose an operation."""
+    # Try to read username and api_token
+    global username, api_token
+    try:
+        with open('config.json', 'r') as f:
+            config = json.load(f)
+            username = config['username']
+            api_token = config['api_token']
+            if username == "" or api_token == "":
+                exit(1)
+            else:
+                print("User name:", username)
+                print("Api token:", api_token)
+    except:
+        print("Failed to read user name and password in config.jon file.")
+        exit(1)
+    # Read list of organizations from file
+    print("\nReading list of organizations from file.\n")
+    org_list = []
+    with open('organizations.txt', 'r') as f:
+        for line in f:
+            # Using rstrip to remove the newline escape sequences
+            org_list.append(line.rstrip('\n'))
+    print("Will scrape data from the following organizations:",*org_list)
+    # Let user specify an operation
+    print("")
+    print("Please choose an operation (you can select multiple options by entering several number separated by commas):\n")
+    print("1. Get a list of the organizations' repositories (CSV)")
+    print("2. Get all contributors of the organizations' repositories as a spreadsheet and as a directed graph (CSV and GEXF).")
+    print("3. Get a list of the repositories of all the members of the organizations (CSV)")
+    print("4. Get information for each member of the organizations (CSV)")
+    print("5. Generate spreadsheet for starred repositories (CSV)")
+    print("6. Generate a full follower network (directed graph) which includes all followers and users followed by the members of the organizations (GEXF)")
+    print("7. Generate a narrow follower network (directed graph) which only includes members of the organizations (GEXF)")
+    print("8. Generate a graph illustrating the membership structures (GEXF)")
+    print("")
+    operations = input()
+    operations_input = operations.split(', ')
+    # TODO: Better function mapping (that is better in handling optional arguments)?
+    operations_dict = {
+    1: get_repos,
+    2: get_contributors,
+    3: get_members_repos,
+    4: get_members_info,
+    5: get_starred_repos,
+    6: generate_follower_network,
+    7: generate_follower_network,
+    8: generate_memberships
+    }
+    for operation in operations_input:
+        if int(operation) == 6:
+            operations_dict[int(operation)](org_list, network_type = "full")
+        elif int(operation) == 7:
+            operations_dict[int(operation)](org_list, network_type = "narrow")
+        else:
+            operations_dict[int(operation)](org_list)
+
+def load_json(url):
+    # TODO: Add error handling in case request fails (e.g. bcs repo was not found)
+    """Helper function to load json file using requests."""
+    r = requests.get(url, auth=(username, api_token))
+    jsonData = json.loads(r.text)
+    return jsonData
+
+def generate_csv(type, json_list, columns_list):
+    """Helper function to write CSV file."""
+    with open("data/"+type+"_"+time.strftime("%Y-%m-%d_%H:%M:%S")+".csv", 'a+') as f:
+        csv_file = csv.DictWriter(f, fieldnames = columns_list, extrasaction="ignore")
+        csv_file.writeheader()
+        if type == "members-info":
+            for member in json_list:
+                csv_file.writerow(member)
+        else:
+            for org in range(len(json_list)):
+                for item in range(len(json_list[org])):
+                    csv_file.writerow(json_list[org][item])
+    print("\nCSV file saved as data/"+type+"_"+time.strftime("%Y-%m-%d_%H:%M:%S")+".csv")
+
+def get_repos(org_list):
+    """Generates a CSV with a list of the organizations' repositories."""
+    jsonRepos = []
+    for org in org_list:
+        print("\nScraping repositories of",org)
+        jsonRepo = load_json("https://api.github.com/orgs/"+org+"/repos?per_page=100")
+        for repo in jsonRepo:
+            # Add field for org to make CSV file more useful
+            repo['organization'] = org
+            # Python 2: Using smart_str to deal with encodings
+            repo['description'] = smart_str(repo['description'])
+        jsonRepos.append(jsonRepo)
+    # Create a list with the items I'm interested in, then call generate_csv function
+    columns_list = ['organization', 'name', 'full_name', 'stargazers_count', 'watchers_count', 'language', 'created_at', 'updated_at', 'homepage', 'fork','description']
+    generate_csv("repo-list", jsonRepos, columns_list)
+
+def get_contributors(org_list):
+    """Generats a CSV listing all contributors to the organizations' repositories."""
+    print("\nCreating list of contributors.")
+    jsonContributor_list = []
+    graph = nx.DiGraph()
+    columns_list = ['organization', 'repository', 'login', 'contributions', 'html_url', 'url']
+    for org in org_list:
+        print('\nScraping contributors of',org)
+        jsonRepo = load_json("https://api.github.com/orgs/"+org+"/repos?per_page=100")
+        for repo in jsonRepo:
+            # try...except to deal with empty repositories
+            try:
+                print("Getting contributors of",repo["name"])
+                # First, add repo as a node to the graph
+                graph.add_node(repo['name'], organization=org)
+                # Then get a list of contributors
+                jsonContributors = load_json("https://api.github.com/repos/"+org+"/"+repo["name"]+"/contributors")
+                for contributor in jsonContributors:
+                    # Add each contributor as an edge to the graph
+                    graph.add_edge(contributor['login'], repo['name'] , organization=org)
+                    # Then prepare CSV and add fields to make it more usable
+                    contributor["organization"] = org
+                    contributor["repository"] = repo["name"]
+                jsonContributor_list.append(jsonContributors)
+            except:
+                # If repository is empty, fill out the rows with "N/A"
+                print("Repository '"+repo["name"]+"' returned an error, possibly because it's empty.")
+                contributor['organization'] = org
+                contributor['repository'] = repo["name"]
+                contributor['login'] = "N/A"
+                contributor['contributions'] = "N/A"
+                contributor['html_url'] = "N/A"
+                contributor['url'] = "N/A"
+            continue
+    generate_csv("contributor-list", jsonContributor_list, columns_list)
+    nx.write_gexf(graph, "data/contributor-network_"+time.strftime("%Y-%m-%d_%H:%M:%S")+'.gexf')
+    print("\nSaved graph file: data/contributor-network_"+time.strftime("%Y-%m-%d_%H:%M:%S")+".gexf")
+
+def get_members_repos(org_list):
+    """Gets a list of all the members of an organization and their repositories."""
+    print("\nGetting repositories of all members.")
+    jsonMembersRepo_list = []
+    columns_list = ['organization', 'user', 'full_name', 'fork', 'stargazers_count', 'watchers_count', 'forks_count', 'language', 'description']
+    for org in org_list:
+        print('\nGetting members of',org)
+        jsonMembers = load_json("https://api.github.com/orgs/"+org+"/members?per_page=100")
+        for member in jsonMembers:
+            print('Getting repositories of',member['login'])
+            jsonMembersRepos = load_json("https://api.github.com/users/"+member['login']+"/repos?per_page=100")
+            for repo in jsonMembersRepos:
+                # Add fields to make CSV file more usable
+                repo['organization'] = org
+                repo['user'] = member['login']
+                # Python 2: Using smart_str to deal with encodings
+                repo['description'] = smart_str(repo['description'])
+            jsonMembersRepo_list.append(jsonMembersRepos)
+    generate_csv("members-list", jsonMembersRepo_list, columns_list)
+
+def get_members_info(org_list):
+    """Creates a CSV with information about the members of the organizations."""
+    print("\nGetting user information of all members.")
+    jsonMembersInfo_list = []
+    columns_list = ['login', 'name', 'url', 'type', 'company', 'blog', 'location', 'email']
+    for org in org_list:
+        print('\nGetting members of',org)
+        jsonMembers = load_json("https://api.github.com/orgs/"+org+"/members?per_page=100")
+        for member in jsonMembers:
+            print("Getting user information for",member["login"])
+            jsonMembers = load_json("https://api.github.com/users/"+member["login"])
+            # Add field to make CSV file more usable
+            jsonMembers["organization"] = org
+            # Python 2: Using smart_str to deal with encodings
+            jsonMembers["location"] = smart_str(jsonMembers['location'])
+            jsonMembers["name"] = smart_str(jsonMembers['name'])
+            jsonMembers["company"] = smart_str(jsonMembers['company'])
+            jsonMembers["email"] = smart_str(jsonMembers['email'])
+            jsonMembersInfo_list.append(jsonMembers)
+    generate_csv("members-info", jsonMembersInfo_list, columns_list)
+
+def get_starred_repos(org_list):
+    """Creates a CSV with all the repositories starred by members of the organizations."""
+    print("\nGetting repositories starred by members.")
+    jsonMembersStarred_list = []
+    columns_list = ['user', 'full_name', 'html_url', 'language', 'description']
+    for org in org_list:
+        print('\nGetting members of',org)
+        jsonMembers = load_json("https://api.github.com/orgs/"+org+"/members?per_page=100")
+        for member in jsonMembers:
+            print('Getting starred repositories of',member['login'])
+            jsonStarred = load_json("https://api.github.com/users/"+member['login']+"/starred")
+            for repo in jsonStarred:
+                repo['user'] = member['login']
+                # Python 2: Using smart_str to deal with encodings
+                repo['description'] = smart_str(repo['description'])
+            jsonMembersStarred_list.append(jsonStarred)
+    generate_csv("starred-list", jsonMembersStarred_list, columns_list)
+
+def generate_follower_network(org_list, network_type = ""):
+    """Gets every user following the members of organizations (followers) and the
+    users they are following themselves (following) and generates a directed graph.
+    Only includes members of specified organizations if network_type == narrow."""
+    if network_type == "full":
+        print("\nGenerating full follower network.")
+    else:
+        print("\nGenerating narrow follower network.")
+        # Getting a list of all members if narrow graph is chosen
+        members_list = []
+        for org in org_list:
+            print("\nGetting members of", org)
+            jsonMembers = load_json("https://api.github.com/orgs/"+org+"/members?per_page=100")
+            for member in jsonMembers:
+                members_list.append(member['login'])
+    graph = nx.DiGraph()
+    for org in org_list:
+        print("\nGetting members of",org)
+        jsonMembers = load_json("https://api.github.com/orgs/"+org+"/members?per_page=100")
+        for member in jsonMembers:
+            jsonMembersFollowers = load_json("https://api.github.com/users/"+member["login"]+"/followers?per_page=100")
+            jsonMembersFollowing = load_json("https://api.github.com/users/"+member["login"]+"/following?per_page=100")
+            print('Getting follower network of',member['login'])
+            graph.add_node(member['login'], organization=org)
+            if network_type == "full":
+                for follower in jsonMembersFollowers:
+                    graph.add_edge(follower['login'], member['login'] , organization=org)
+                for following in jsonMembersFollowing:
+                    graph.add_edge(member['login'], following['login'], organization=org)
+            else: # Generate narrow network by only including members of organizations
+                for follower in jsonMembersFollowers:
+                    if follower['login'] in members_list:
+                        graph.add_edge(follower['login'], member['login'] , organization=org)
+                for following in jsonMembersFollowing:
+                    if following['login'] in members_list:
+                        graph.add_edge(member['login'], following['login'], organization=org)
+    nx.write_gexf(graph, "data/"+network_type+"-follower-network_"+time.strftime("%Y-%m-%d_%H:%M:%S")+'.gexf')
+    print("\nSaved graph file: data/"+network_type+"-follower-network_"+time.strftime("%Y-%m-%d_%H:%M:%S")+".gexf")
+
+def generate_memberships(org_list):
+    """Takes all the members of the organizations and generates a directed graph
+    showing all their memberships."""
+    print("\nGenerating network of memberships.")
+    graph = nx.DiGraph()
+    for org in org_list:
+        jsonMembers = load_json("https://api.github.com/orgs/"+org+"/members?per_page=100")
+        for member in jsonMembers:
+            print("Getting membership of", member['login'])
+            graph.add_node(member['login'], node_type='user')
+            jsonMemberships = load_json("https://api.github.com/users/"+member['login']+"/orgs")
+            for organization in jsonMemberships:
+                graph.add_edge(member['login'], organization['login'], node_type='organization')
+    nx.write_gexf(graph, 'data/membership-network_'+time.strftime("%Y-%m-%d_%H:%M:%S")+'.gexf')
+    print("\nSaved graph file: data/membership-network_"+time.strftime("%Y-%m-%d_%H:%M:%S")+".gexf")
+
+start()
